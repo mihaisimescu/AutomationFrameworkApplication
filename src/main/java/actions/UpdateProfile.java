9b6b95596b5a1ff449a3d26fa1dd3014b5a71360
MihaiSimescu <mihai.simescu@emeal.nttdata.com>
2024-07-09 11:54:00 +0300

    Implemented Update Profile test.

--- src/main/java/actions/UpdateProfile.java
+++ src/main/java/actions/UpdateProfile.java
@@ -14,4 +14,47 @@ public class UpdateProfile {
     public String getUpdateContactTitle(){
         return elements.getUpdateContactInfo().getText();
     }
+
+    public void enterFirstName(String firstName){
+        elements.firstName().clear();
+        elements.firstName().sendKeys(firstName);
+    }
+
+    public void enterLastName(String lastName){
+        elements.lastName().clear();
+        elements.lastName().sendKeys(lastName);
+    }
+
+    public void enterAddress(String address){
+        elements.address().clear();
+        elements.address().sendKeys(address);
+    }
+
+    public void enterCity(String city){
+        elements.city().clear();
+        elements.city().sendKeys(city);
+    }
+
+    public void enterState(String state){
+        elements.state().clear();
+        elements.state().sendKeys(state);
+    }
+
+    public void enterZipCode(String zipCode){
+        elements.zipCode().clear();
+        elements.zipCode().sendKeys(zipCode);
+    }
+
+    public void enterPhone(String phone){
+        elements.phoneNumber().clear();
+        elements.phoneNumber().sendKeys(phone);
+    }
+
+    public void clickUpdateProfile(){
+        elements.updateProfile().click();
+    }
+
+    public String getUpdateProfileResult(){
+        return elements.getUpdateContactInfoResult().getText();
+    }
 }
